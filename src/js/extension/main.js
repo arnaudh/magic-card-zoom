bb88e089b6f2c50f51ff35d8533cc07e9e1349c5
arnaudh <arnaudh@users.noreply.github.com>
2019-11-11 18:59:19 -0500

    Cleanup main.js
    var -> let + remove DOM elements when stopping MCZ

--- src/js/extension/main.js
+++ src/js/extension/main.js
@@ -7,25 +7,25 @@ const config = require('../../../config.json');
 
 let channelUrlRegex = /.*youtube\.com\/channel\/([a-zA-Z0-9_-]+)/;
 
-var theVideo;
-var theVideoSource;
-var theCanvas;
-var mousePos;
-var currentRectangle;
-var popup;
-var popupImage;
-var loadingPopup;
-var loadingPopupImage;
-var parentOfPopup;
-var body;
+let theVideo;
+let theVideoSource;
+let theCanvas;
+let mousePos;
+let currentRectangle;
+let popup;
+let popupImage;
+let loadingPopup;
+let loadingPopupImage;
+let parentOfPopup;
+let body;
 
-var mousePosition = null;
-var lastMouseLoopPosition = null;
-var lastMouseStopToIdentifyPosition = null;
-var mouse_stopped_to_identify = false;
-var lastMove = new Date();
-var identifyRequestId = null;
-var channelID;
+let mousePosition = null;
+let lastMouseLoopPosition = null;
+let lastMouseStopToIdentifyPosition = null;
+let mouse_stopped_to_identify = false;
+let lastMove = new Date();
+let identifyRequestId = null;
+let channelID;
 
 
 // how long must the mouse stay fixed over the video to trigger identify()
@@ -37,14 +37,14 @@ const spaceX = 30;
 // css Px
 const MOVE_DISTANCE_TO_CANCEL_IDENTIFICATION = 5;
 
-var waitingForIdentifyResponseMessageID = null;
-var lastMessageSent = null;
+let waitingForIdentifyResponseMessageID = null;
+let lastMessageSent = null;
 
-var checkMouseLoopInterval;
-var readyStateCheckInterval = setInterval(function() {
+let checkMouseLoopInterval;
+let readyStateCheckInterval = setInterval(function() {
   console.log('[MCZ main.js] Checking if there is a video...');
   body = document.getElementsByTagName('body')[0];
-  var videos = document.getElementsByTagName('video');
+  let videos = document.getElementsByTagName('video');
   if (videos.length > 0) {
     clearInterval(readyStateCheckInterval);
 
@@ -85,13 +85,13 @@ var readyStateCheckInterval = setInterval(function() {
 }, 10);
 
 
-var messageID = 0;
+let messageID = 0;
 
 function addKeyListener() {
   document.onkeypress = function(e) {
     e = e || window.event;
-    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-    var char = String.fromCharCode(charCode)
+    let charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+    let char = String.fromCharCode(charCode)
     console.log('KEY PRESS', char)
     switch (char) {
       case 'y':
@@ -140,7 +140,7 @@ function checkMouseLoop() {
     // from there on, we shouldn't ask to identify unless we move again
     mouse_stopped_to_identify = true;
 
-    var data = {
+    let data = {
       mousePosition: mousePosition,
       videoBoundingRect: theVideo.getBoundingClientRect(),
       devicePixelRatio: window.devicePixelRatio,
@@ -210,7 +210,7 @@ function messageListener(message, sender, sendResponse) {
   switch (message.messageType) {
     case 'stopMainLoop':
       clearInterval(checkMouseLoopInterval);
-      hidePopupAndLoadingGif();
+      removePopupAndLoadingGif();
       document.removeEventListener('mousemove', mymousemove, false);
       chrome.runtime.onMessage.removeListener(messageListener);
       break;
@@ -278,6 +278,12 @@ function hidePopupAndLoadingGif() {
   loadingPopup.style.visibility = "hidden";
 }
 
+function removePopupAndLoadingGif() {
+  console.log('remove popup');
+  popup.parentNode.removeChild(popup);
+  loadingPopup.parentNode.removeChild(loadingPopup);
+}
+
 function showCard(card_url) {
   popupImage.onload = function() {
     loadingPopup.style.visibility = "hidden";
@@ -295,7 +301,7 @@ function showLoadingGif() {
 
 function getChannelId() {
   let l = document.links;
-  for(var i=0; i<l.length; i++) {
+  for(let i=0; i<l.length; i++) {
     let match = channelUrlRegex.exec(l[i].href);
     if (match) {
       let channelID = match[1];
