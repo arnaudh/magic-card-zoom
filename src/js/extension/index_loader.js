0691a80308a8ba6717ac640d6c4248f6a4832eb2
arnaudh <arnaudh@users.noreply.github.com>
2020-02-23 20:53:45 -0500

    Exclude basic lands

--- src/js/extension/index_loader.js
+++ src/js/extension/index_loader.js
@@ -1,11 +1,19 @@
 const {fetch} = require('fetch-ponyfill')();
+const config = require("../../../config.json");
+const basicLands = require("../../../assets/metadata/cards/basic_lands.json");
 
 module.exports.load = async function(mtg_sets) {
     return await Promise.all(mtg_sets.map(s => fetch(`assets/indexes/${s}.json`)))
         .then(responses => {
             let jsons = Promise.all(responses.map(r => r.json()));
             return jsons.then(jj => {
-                var result = Object.assign(...jj);
+                let result = Object.assign(...jj);
+                if (!config.includeBasicLands) {
+                    let lengthBefore = Object.keys(result).length;
+                    result = Object.fromEntries(Object.entries(result).filter(([k,v]) => !basicLands.includes(k)));
+                    let lengthAfter = Object.keys(result).length;
+                    console.log(`Removing basic lands: drops the number of cards from ${lengthBefore} to ${lengthAfter}`);
+                }
                 return result;
             })
         });
