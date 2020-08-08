4724d5d53c2de1df524189dde122ff5a7c8518c7
Arnaud Henry <arnaud.henry@invenialabs.co.uk>
2020-08-08 16:49:55 +0100

    Fix download_all script by calling require in sequence
    This is needed because download_standard_info and
    download_images_and_metadata both need the sets.json that gets
    downloaded in download_mtg_sets_metadata

--- src/js/util/download_all.js
+++ src/js/util/download_all.js
@@ -1,9 +1,5 @@
 
-downloadMtgSetsMetadata = require('./download_mtg_sets_metadata.js');
-downloadStandardInfo = require('./download_standard_info.js');
-downloadImagesAndMetadata = require('./download_images_and_metadata.js');
-
 Promise.resolve()
-    .then(downloadMtgSetsMetadata)     // all sets info (code, name, etc)
-    .then(downloadStandardInfo)        // list of sets per standard
-    .then(downloadImagesAndMetadata);  // images and their urls
+    .then(require('./download_mtg_sets_metadata.js'))     // all sets info (code, name, etc)
+    .then(require('./download_standard_info.js'))        // list of sets per standard
+    .then(require('./download_images_and_metadata.js'));  // images and their urls
