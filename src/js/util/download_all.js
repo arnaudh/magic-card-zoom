3764af84efc823cf98ec440a60a2e98edac7ea5d
arnaudh <arnaudh@users.noreply.github.com>
2022-07-28 23:15:39 +0100

    Add sets up to dmu

--- src/js/util/download_all.js
+++ src/js/util/download_all.js
@@ -1,5 +1,5 @@
 
 Promise.resolve()
     .then(require('./download_mtg_sets_metadata.js'))     // all sets info (code, name, etc)
-    .then(require('./download_standard_info.js'))        // list of sets per standard
+    .then(require('./download_standard_info.js'))         // list of sets per standard
     .then(require('./download_images_and_metadata.js'));  // images and their urls
